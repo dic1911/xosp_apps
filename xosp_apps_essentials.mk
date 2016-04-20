a9740b2886d69c537c2ca5130c1eeb5f4ffe8c97
Calin <nilac8991@gmail.com>
2016-04-20 16:57:25 +0200

    Fix path
    Signed-off-by: Calin <nilac8991@gmail.com>

--- xosp_apps_essentials.mk
+++ xosp_apps_essentials.mk
@@ -1,6 +1,6 @@
 #Copy essentials XOSPApps
 
 PRODUCT_COPY_FILES += \
-	essentials/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
-	essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-	essentials/Home/Home.apk:system/app/Home/Home.apk
+	xosp_apps/essentials/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
+	xosp_apps/essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
+	xosp_apps/essentials/Home/Home.apk:system/app/Home/Home.apk
