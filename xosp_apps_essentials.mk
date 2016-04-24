6bb45a1ca1d6bd45259f7101750799167f0d8947
Calin <nilac8991@gmail.com>
2016-04-24 12:23:56 +0200

    Add IS_ARM64 flag
    Signed-off-by: Calin <nilac8991@gmail.com>

--- xosp_apps_essentials.mk
+++ xosp_apps_essentials.mk
@@ -1,6 +1,16 @@
 #Copy essentials XOSPApps
 
+ifeq ($(IS_ARM64), TRUE)
+
+PRODUCT_COPY_FILES += \
+	xosp_apps/arm64/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
+	xosp_apps/arm64/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so
+else
+
 PRODUCT_COPY_FILES += \
 	xosp_apps/essentials/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
-	xosp_apps/essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
+	xosp_apps/essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so
+endif
+
+PRODUCT_COPY_FILES += \
 	xosp_apps/essentials/Home/Home.apk:system/app/Home/Home.apk
