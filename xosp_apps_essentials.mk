f1f419f7ee9c8a6998cf6b553218f886af322744
Calin <nilac8991@gmail.com>
2016-07-11 15:34:48 +0200

    Fix derp
    Signed-off-by: Calin <nilac8991@gmail.com>

--- xosp_apps_essentials.mk
+++ xosp_apps_essentials.mk
@@ -16,15 +16,15 @@ else
 PRODUCT_COPY_FILES += \
 	xosp_apps/essentials/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
 	xosp_apps/essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-	xosp_apps/arm64/Album/lib/arm/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
-	xosp_apps/arm64/Album/lib/arm/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
-	xosp_apps/arm64/Album/lib/arm/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
-	xosp_apps/arm64/Album/lib/arm/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
-	xosp_apps/arm64/Album/lib/arm/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
-	xosp_apps/arm64/Album/lib/arm/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so \
-	xosp_apps/arm64/Album/lib/arm/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
-	xosp_apps/arm64/Album/lib/arm/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
-	xosp_apps/arm64/Album/lib/arm/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so 
+	xosp_apps/essentials/Album/lib/arm/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
+	xosp_apps/essentials/Album/lib/arm/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
+	xosp_apps/essentials/Album/lib/arm/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
+	xosp_apps/essentials/Album/lib/arm/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
+	xosp_apps/essentials/Album/lib/arm/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
+	xosp_apps/essentials/Album/lib/arm/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so \
+	xosp_apps/essentials/Album/lib/arm/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
+	xosp_apps/essentials/Album/lib/arm/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
+	xosp_apps/essentials/Album/lib/arm/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so 
 endif
 
 PRODUCT_COPY_FILES += \
