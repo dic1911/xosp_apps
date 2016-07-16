63b4beb98afc5e9a0a725fdf7dcfb067a5196142
DiC1911 <d69873@gmail.com>
2016-07-16 11:12:21 +0800

    Fix Album FC (2)

--- xosp_apps_essentials.mk
+++ xosp_apps_essentials.mk
@@ -5,26 +5,26 @@ ifeq ($(IS_ARM64), TRUE)
 PRODUCT_COPY_FILES += \
 	xosp_apps/arm64/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
 	xosp_apps/arm64/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-	xosp_apps/arm64/Album/lib/arm/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
-	xosp_apps/arm64/Album/lib/arm/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
-	xosp_apps/arm64/Album/lib/arm/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
-	xosp_apps/arm64/Album/lib/arm/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
-	xosp_apps/arm64/Album/lib/arm/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
-	xosp_apps/arm64/Album/lib/arm/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so 
+	xosp_apps/arm64/Album/lib/arm/libgifreader.so:system/lib/libgifreader.so \
+	xosp_apps/arm64/Album/lib/arm/libimagecalc.so:system/lib/libimagecalc.so \
+	xosp_apps/arm64/Album/lib/arm/libmaketc.so:system/lib/libmaketc.so \
+	xosp_apps/arm64/Album/lib/arm/libsceniccore.so:system/lib/libsceniccore.so \
+	xosp_apps/arm64/Album/lib/arm/libspf.so:system/lib/libspf.so \
+	xosp_apps/arm64/Album/lib/arm/libSQHashGenerator.so:system/lib/libSQHashGenerator.so 
 else
 
 PRODUCT_COPY_FILES += \
 	xosp_apps/essentials/textinput-tng/textinput-tng.apk:system/priv-app/textinput-tng/textinput-tng.apk \
 	xosp_apps/essentials/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/priv-app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-	xosp_apps/essentials/Album/lib/arm/libgifreader.so:system/priv-app/Album/lib/arm/libgifreader.so \
-	xosp_apps/essentials/Album/lib/arm/libimagecalc.so:system/priv-app/Album/lib/arm/libimagecalc.so \
-	xosp_apps/essentials/Album/lib/arm/libmaketc.so:system/priv-app/Album/lib/arm/libmaketc.so \
-	xosp_apps/essentials/Album/lib/arm/libsceniccore.so:system/priv-app/Album/lib/arm/libsceniccore.so \
-	xosp_apps/essentials/Album/lib/arm/libspf.so:system/priv-app/Album/lib/arm/libspf.so \
-	xosp_apps/essentials/Album/lib/arm/libSQHashGenerator.so:system/priv-app/Album/lib/arm/libSQHashGenerator.so \
-	xosp_apps/essentials/Album/lib/arm/libdrmstream_depjb.so:system/priv-app/Album/lib/arm/libdrmstream_depjb.so \
-	xosp_apps/essentials/Album/lib/arm/libjpeguser.so:system/priv-app/Album/lib/arm/libjpeguser.so \
-	xosp_apps/essentials/Album/lib/arm/libnativeio.so:system/priv-app/Album/lib/arm/libnativeio.so 
+	xosp_apps/essentials/Album/lib/arm/libgifreader.so:system/lib/libgifreader.so \
+	xosp_apps/essentials/Album/lib/arm/libimagecalc.so:system/lib/libimagecalc.so \
+	xosp_apps/essentials/Album/lib/arm/libmaketc.so:system/lib/libmaketc.so \
+	xosp_apps/essentials/Album/lib/arm/libsceniccore.so:system/lib/libsceniccore.so \
+	xosp_apps/essentials/Album/lib/arm/libspf.so:system/lib/libspf.so \
+	xosp_apps/essentials/Album/lib/arm/libSQHashGenerator.so:system/lib/libSQHashGenerator.so \
+	xosp_apps/essentials/Album/lib/arm/libdrmstream_depjb.so:system/lib/libdrmstream_depjb.so \
+	xosp_apps/essentials/Album/lib/arm/libjpeguser.so:system/lib/libjpeguser.so \
+	xosp_apps/essentials/Album/lib/arm/libnativeio.so:system/lib/libnativeio.so 
 endif
 
 PRODUCT_COPY_FILES += \
